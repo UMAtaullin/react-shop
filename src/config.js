0baacb8472b0826b24405e7d8fb370a7bc6a9001
UMAtaullin <ataullinural@gmail.com>
2025-02-09 16:20:36 +0500

    fix: api_key

--- src/config.js
+++ src/config.js
@@ -1,4 +1,7 @@
-const API_KEY = '4c91c8c1-38e3cd66-dc2db521-d12fb217'
+const API_KEY = import.meta.env.VITE_API_KEY;
+if (!API_KEY) {
+  console.error("API_KEY is not defined in .env.local");
+}
 
 const API_URL = 'https://fortniteapi.io/v2/shop?lang=en'
 
